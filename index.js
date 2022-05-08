988b82b916ba62b28a57b5f2432d13c69fd17291
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-05-08 20:44:43 +0600

    implemented jsonwebtoken and complete assignment project server

--- index.js
+++ index.js
@@ -4,33 +4,48 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.aplit(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+  });
+  next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l4qxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("wareHouse").collection("carService");
-    
 
-   // jsonwebtoken access api
-   app.post('/login', async(req, res) =>{
-     const user = req.body;
-     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-       expiresIn: '20d'
-     });
-     res.send({accessToken});
-   }) 
-    
+    // jsonwebtoken access api
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "20d",
+      });
+      res.send({ accessToken });
+    });
 
     //data get from mongodb database
     app.get("/service", async (req, res) => {
@@ -41,66 +56,76 @@ async function run() {
     });
 
     // post
-    app.post('/service', async(req, res) =>{
+    app.post("/service", async (req, res) => {
       const newService = req.body;
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
 
     // get service by email
-    app.get("/serviceByEmail/:email", async (req, res) => {
+    app.get("/serviceByEmail/:email", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.params.email;
-      const query = {email: email}
-      const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      }
+      else{
+        res.status(403).send({message: 'forbidde access'})
+      }
     });
 
-    app.get('/service/:id', async(req, res) =>{
+    app.get("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
 
     // Delete my items and manage items
-    app.delete('/service/:id', async(req, res) =>{
+    app.delete("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
-    
 
     // use put for update quantity
-    app.put('/service/:id', async(req, res) =>{
+    app.put("/service/:id", async (req, res) => {
       const id = req.params.id;
-      const {quantity} = req.body;
-      const filter = {_id: ObjectId(id)};
+      const { quantity } = req.body;
+      const filter = { _id: ObjectId(id) };
       const options = { upsert: true };
       const updatedDoc = {
-        $set: {quantity: quantity}
+        $set: { quantity: quantity },
       };
-      const result = await serviceCollection.updateOne(filter, updatedDoc, options);
-      res.send(result);
-    });
-    
-    // use put method for delivered 1service per click
-    app.put('/delivered/:id', async(req, res) =>{
-      const id = req?.params?.id;
-      const {quantity} = req?.body;
-      const filter = {_id: ObjectId(id)};
-      const options = { upsert: true };
-      const updatedDoc = {
-        $set: {quantity: quantity}
-      };
-      const result = await serviceCollection.updateOne(filter, updatedDoc, options);
+      const result = await serviceCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
 
-    
-  } 
-  finally {
+    // use put method for delivered 1service per click
+    app.put("/delivered/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const { quantity } = req?.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: { quantity: quantity },
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
   }
 }
 run().catch(console.dir);
