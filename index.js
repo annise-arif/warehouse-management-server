96d50c8943936ac445cb5d2f2910194f5081ab5b
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-05-08 23:30:47 +0600

    heroku deploy

--- index.js
+++ index.js
@@ -10,21 +10,22 @@ const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-  const token = authHeader.aplit(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "forbidden access" });
-    }
-    console.log("decoded", decoded);
-    req.decoded = decoded;
-  });
-  next();
-}
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).send({ message: "forbidden access" });
+//     }
+//     // console.log("decoded", decoded);
+//     req.decoded = decoded;
+//     next();
+//   });
+  
+// }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l4qxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -38,14 +39,14 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("wareHouse").collection("carService");
 
-    // jsonwebtoken access api
-    app.post("/login", async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "20d",
-      });
-      res.send({ accessToken });
-    });
+    // // jsonwebtoken access api
+    // app.post("/login", async (req, res) => {
+    //   const user = req.body;
+    //   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "20d",
+    //   });
+    //   res.send({ accessToken });
+    // });
 
     //data get from mongodb database
     app.get("/service", async (req, res) => {
@@ -63,18 +64,14 @@ async function run() {
     });
 
     // get service by email
-    app.get("/serviceByEmail/:email", verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
-      const email = req.params.email;
-      if (email === decodedEmail) {
+    app.get("/serviceByEmail/:email", async (req, res) => {
+
+        const email = req.params.email;
         const query = { email: email };
         const cursor = serviceCollection.find(query);
         const services = await cursor.toArray();
         res.send(services);
-      }
-      else{
-        res.status(403).send({message: 'forbidde access'})
-      }
+      
     });
 
     app.get("/service/:id", async (req, res) => {
